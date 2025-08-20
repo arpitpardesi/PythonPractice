dd0acce16aae2a13bcb413ca27b3fea988bce5b6
Arpit Pardesi <arpit.pardesi6@gmail.com>
2025-08-20 23:00:43 +0530

    iterator

---
+++ Miscellenous/Iterators/ite.py
@@ -0,0 +1,14 @@
+it = iter([1,2,3, 4])
+print (next(it))
+print (it.__next__())
+print (it.__next__())
+print (next(it))
+
+it = iter([1,2,3, 4, 5])
+print (next(it))
+while True:
+   try:
+      no = next(it)
+      print (no)
+   except StopIteration:
+      break
