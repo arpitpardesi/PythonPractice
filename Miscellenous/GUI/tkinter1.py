c31a67bf92a87b02bf83e7b257ca77a03ba83848
Arpit Pardesi <arpit.pardesi6@gmail.com>
2025-08-25 23:28:04 +0530

    GUI - tkinter

---
+++ Miscellenous/GUI/tkinter1.py
@@ -0,0 +1,18 @@
+from tkinter.simpledialog import askinteger
+from tkinter import *
+from tkinter import messagebox
+
+top = Tk()
+top.title("Hello")
+top.geometry("200x100")
+
+
+def show():
+    num = askinteger("Input", "Input an Integer")
+    print(num)
+
+
+B = Button(top, text="Click", command=show)
+B.place(x=50, y=50)
+
+top.mainloop()
